34018e79ef21c14bcc02cb8f5bcccfcd2061fcce
Mateusz <nazarowy96@gmail.com>
2020-11-11 13:06:13 +0100

    Added PHP files and content changing by PHP

---
+++ kontakt.php
@@ -0,0 +1,25 @@
+<h2>Kontakt</h2>
+              <div class="row">
+                <div class="col-12">
+                  <form>
+                      <div class="form-group">
+                        <label for="email">Adres Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                      </div>
+                      <div class="form-group">
+                          <label for="imie">Imię</label>
+                          <input type="text" name="imie" id="imie" class="form-control">
+                      </div>
+                      <div class="form-group">
+                        <label for="wiadomosc">Wiadomość</label>
+                        <textarea name="wiadomosc" id="wiadomosc" rows="5" class="form-control"></textarea>
+                      </div>
+                      <button type="submit" class="btn" id="wyslij">Wyślij</button>
+                  </form>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 map">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d19470.254112517636!2d16.895676996388328!3d52.41063134902064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1skawiarnia%20pozna%C5%84!5e0!3m2!1spl!2spl!4v1604590415137!5m2!1spl!2spl" width="100%" height="300" frameborder="0" style="border: 5px solid #c9c9c9;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+              </div>
---
+++ main.php
@@ -0,0 +1,4 @@
+<h2>Kawiarnia</h2>
+              <p>Lokal gastronomiczny będący przede wszystkim miejscem spotkań towarzyskich. Podawane są tam głównie napoje i dania deserowe, głównie kawa, zazwyczaj również: ciastka, lody, herbata, czy wino.
+              Lokal gastronomiczny będący przede wszystkim miejscem spotkań towarzyskich. Podawane są tam głównie napoje i dania deserowe, głównie kawa, zazwyczaj również: ciastka, lody, herbata, czy wino.</p>
+              <img src="img/wide.jpg" alt="Kawiarnia" id="wide">
